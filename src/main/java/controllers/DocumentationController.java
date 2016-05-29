6659ab9a146022de31871ea50c2d48e81cda23f8
Dr_Linus <tony972psg@hotmail.fr>
2016-05-29 23:05:29 +0200

    Add the documentation part.

---
+++ src/main/java/controllers/DocumentationController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Path;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.GET;
+import java.io.IOException;
+
+
+    @Path("/documentation")
+    public class DocumentationController extends HttpServlet{
+
+        @GET
+        public void getHome(@Context HttpServletRequest request,
+                            @Context HttpServletResponse response) throws ServletException, IOException {
+            request.getRequestDispatcher("/WEB-INF/views/documentation.html")
+                    .forward(request, response);
+        }
+
+    }
